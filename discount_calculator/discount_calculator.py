2d06e5c4a872950130815c429d5c6c0382d000ac
Gaius Emmanuel <gaiusemmanuel12@gmail.com>
2026-07-19 11:08:45 +0100

    Implement basic discount calculator using OOP

--- discount_calculator/discount_calculator.py
+++ discount_calculator/discount_calculator.py
@@ -1,1 +1,30 @@
-print("Hello, World!")
+from abc import ABC, abstractmethod
+
+class Product:
+    def __init__(self, name: str, price: float) -> None:
+        self.name = name
+        self.price = price
+
+class DiscountStrategy(ABC):
+    @abstractmethod
+    def is_applicable(self, product: Product, user_tier: str) -> bool:
+        pass
+
+    @abstractmethod
+    def apply_discount(self, product: Product) -> float:
+        pass
+
+class PercentageDiscount(DiscountStrategy):
+    def __init__(self, percent: int) -> None:
+        self.percent = percent
+
+    def is_applicable(self, product: Product) -> bool:
+        return product.price <= 70
+    
+    def apply_discount(self, product: Product) -> float:
+        return product.price * (1 - self.percent / 100)
+
+product = Product("Laptop", 1000)
+
+discount = PercentageDiscount(25)
+print(discount.apply_discount(product))
